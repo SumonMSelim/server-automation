685ccb78412574f9227e6aabc4395d2a87eff731
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2020-08-06 00:56:17 +0200

    Update certbot-setup.sh

--- certbot-setup.sh
+++ certbot-setup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Adding ppa for Let's Encrypt certbot..."
-sudo add-apt-repository -y ppa:certbot/certbot
 sh ./update.sh
 
 echo "Installing certbot..."
-sudo apt-get install -y certbot python-certbot-nginx
+sudo apt-get install -y certbot python3-certbot-nginx
 
 echo "Generate DH parameters with OpenSSL..."
 sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
