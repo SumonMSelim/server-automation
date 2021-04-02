773ef4ca53b93a7b884595fca1aef3c722b78a76
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2021-04-02 03:27:47 +0200

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -5,7 +5,7 @@ sudo apt install zsh -y
 
 echo "Installing oh-my-zsh..."
 curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh
+sh install.sh --unattended
 
 echo "Preparing default zsh configuration..."
 truncate --size=0 ${HOME}/.zshrc
@@ -14,5 +14,3 @@ rm -f install.sh
 
 echo "Set default shell to zsh"
 chsh -s /bin/zsh
-
-source ~/.zshrc
