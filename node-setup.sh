ad2d79705c7297bd73da7afd043edd77fa258699
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2021-09-02 23:49:45 +0200

    Fix node-setup.sh

--- node-setup.sh
+++ node-setup.sh
@@ -6,12 +6,12 @@ echo "Enter the Node version you want to install..."
 node_versions=( 14 16 )
 
 select node_version in "${node_versions[@]}"; do
-    echo "You have chosen $node_version"
+    echo "You have chosen Node $node_version"
     break
 done
 
 echo "Installing Node ${node_version}..."
-curl -sL https://deb.nodesource.com/setup_${node_version}.x | sudo -E zsh -
+curl -sL https://deb.nodesource.com/setup_${node_version}.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "$(node -v)"
